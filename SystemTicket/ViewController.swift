93a1b7cb27b4c27788b20fa7d1b30e808ec33ac1
Smoks <dd>
2017-11-13 15:07:32 +0300

    my first project in swift

--- SystemTicket/ViewController.swift
+++ SystemTicket/ViewController.swift
@@ -7,21 +7,84 @@
 //
 
 import Cocoa
+import Alamofire
+import SwiftyJSON
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSTableViewDataSource, NSTabViewDelegate {
+    @IBOutlet weak var viewTable: NSTableView!
+    @IBOutlet weak var clickTable: NSTextField!
+    
+    var countColum = -1
+    var timeTable = ["","",""]
+    var baseArray = [[String]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        requestsystemticket()
     }
-
+    
+    public func requestsystemticket() -> Bool
+    {
+        Alamofire.request("http://10.0.0.65/ticket2/alamofire.php").responseJSON { response in
+            if let json = response.result.value {
+                let jsons = JSON(json)
+                var xCode = 0
+                var tempArray = [String]()
+                while xCode <= jsons.count - 1
+                {
+                    if let userNid = jsons[xCode][0].string {
+                        tempArray.append(userNid)
+                    }
+                    if let userFio = jsons[xCode][1].string {
+                        tempArray.append(userFio)
+                    }
+                    if let userPri = jsons[xCode][2].string {
+                        tempArray.append(userPri)
+                    }
+                    xCode += 1
+                    self.baseArray.append(tempArray)
+                    tempArray = [String]()
+                    
+                }
+                self.viewTable.reloadData()
+                
+            }
+        }
+            return true
+    }
+    
+    public func numberOfRows(in tableView: NSTableView) -> Int
+    {
+        clickTable.stringValue = String(baseArray.count)
+        return baseArray.count
+    }
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any?
+    {
+        
+        let xmk = baseArray[row].count - 1
+        if self.countColum == xmk { self.countColum = -1 } else { self.countColum += 1 }
+        if self.countColum == -1
+        {
+            self.timeTable = baseArray[row]
+            self.countColum = 0
+        }
+        if(self.timeTable == ["","",""]) { self.timeTable = baseArray[row] }
+        return timeTable[countColum]
+    }
+    
+    
+    
+    
+    
+    
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    
 }
 
