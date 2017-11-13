476e5542d02f400884778995c48dda3e9febc8df
Smoks <dd>
2017-11-13 13:56:21 +0300

    Initial Commit

---
+++ SystemTicket/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SystemTicket
+//
+//  Created by Admin on 13.11.2017.
+//  Copyright © 2017 Smoks. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
